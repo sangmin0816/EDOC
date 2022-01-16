408a3d2d4e52288a95d83c7c8bde5e3818f66d91
Rabi <sangmin990816@gmail.com>
2022-01-16 23:31:14 +0900

    220116

--- d220116/q1010.java
+++ d220116/q1010.java
@@ -1,8 +1,8 @@
-
+package d220116;
 import java.util.*;
 
 public class q1010 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // https://www.acmicpc.net/problem/1010 다리놓기
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
@@ -13,13 +13,12 @@ public class q1010 {
             int den = 1;
             int num = 1;
 
-            for(int i=N-M+1; i<N+1; i++){
-                num*=i;
-                den*=M;
-                M--;
+            for(int i=N; i>(N-M); i--){
+                num=num*i/den;
+                den++;
             }
 
-            System.out.println(num/den);
+            System.out.println(num);
         }
         
         sc.close();
